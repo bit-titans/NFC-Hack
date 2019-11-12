98d23ee9d26055e45f96e306e8b69dc04f4d1652
abhishekbn87 <abhishekbn87@gmail.com>
2019-11-12 12:02:00 +0530

    Added No Connection Fragment

--- app/src/main/java/com/example/nfc_hack/NoConnection.kt
+++ app/src/main/java/com/example/nfc_hack/NoConnection.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_no_connection.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +20,13 @@ class NoConnection : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_no_connection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_connection.setOnClickListener {
+            findNavController().navigate(R.id.action_noConnection_to_scanFragment)
+        }
     }
 
 
