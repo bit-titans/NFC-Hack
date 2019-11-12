853eb919d9685313a28371e13aec439c6d5d47bb
rottiakash <rottiakash@gmail.com>
2019-11-12 11:35:17 +0530

    First Commit

---
+++ app/src/main/java/com/example/nfc_hack/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.nfc_hack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var navController:NavController  = Navigation.findNavController(this,R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.logout -> {
+                println("You clicked logout")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
