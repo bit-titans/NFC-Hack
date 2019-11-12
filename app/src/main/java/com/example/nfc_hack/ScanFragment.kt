6c240cfe6b08a98070d03c57fdb6ff8ee4dac8f4
anandteerth1999 <anandmathad446@gmail.com>
2019-11-12 13:02:14 +0530

    added auth

--- app/src/main/java/com/example/nfc_hack/ScanFragment.kt
+++ app/src/main/java/com/example/nfc_hack/ScanFragment.kt
@@ -6,18 +6,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 /**
  * A simple [Fragment] subclass.
  */
 class ScanFragment : Fragment() {
-
+    companion object{private const val RC_SIGN_IN = 123}
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private var subs = arrayOfNulls<String>(8)
+    private var sub_name = arrayOfNulls<String>(8)
+    private var sub_date = arrayOfNulls<String>(8)
+    private var sub_time = arrayOfNulls<String>(8)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var id : String
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+        if(currentUser == null){
+            // Inflate the layout for this fragment
+            // Choose authentication providers
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().setAllowNewAccounts(false).build()
+            )
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
+
     }
 
 
